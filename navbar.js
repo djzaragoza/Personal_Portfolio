74e45ee039305aa10f13fcf7f6d4412dbcf465cd
unknown <dj.zaragoza@icloud.com>
2019-11-22 09:42:51 -0800

    created navbar in React

--- navbar.js
+++ navbar.js
@@ -1,1 +1,34 @@
-import React from 'react';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <NavLink
+        exact
+        activeClassName="navbar_link--active"
+        className="navbar_link"
+        to="/"
+      >
+        Home
+      </NavLink>
+      <NavLink
+        activeClassName="navbar_link--active"
+        className="navbar_link"
+        to="/CafePlz"
+      >
+        CafePlz
+      </NavLink>
+      <NavLink
+        activeClassName="navbar_link--active"
+        className="navbar_link"
+        to="/Resume"
+      >
+        Resume
+      </NavLink>
+    </nav>
+  );
+};
+
+export default Navbar;
