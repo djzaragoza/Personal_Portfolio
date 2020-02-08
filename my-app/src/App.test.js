f9d3af7acd9172dad91bc266a8b2df90f482014e
unknown <dj.zaragoza@icloud.com>
2020-02-08 08:23:02 -0800

    created react app files

---
+++ my-app/src/App.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {
+  render
+} from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  const {
+    getByText
+  } = render( < App / > );
+  const linkElement = getByText(/www.djzaragoza.dev/i);
+  expect(linkElement).toBeInTheDocument();
+});
